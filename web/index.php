8ca485750d4292e1c69002d073d4bc330cbad218
Tomas Jenej <shamot@neonus.sk>
2014-02-25 07:50:50 +0100

    Initial commit

---
+++ web/index.php
@@ -0,0 +1,29 @@
+<?php
+
+use Neonus\Microblog\Microblog;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/../views',
+));
+
+$app['debug'] = true;
+
+$blog = new Microblog(__DIR__ . '/../data/');
+
+$app->get('/', function() use ($app, $blog) {
+    return $app['twig']->render('index.twig', array(
+        'posts' => $blog->getPosts(),
+    ));
+});
+
+$app->get('/blog/{url}', function($url) use ($app, $blog) {
+    $post = $blog->getPostByUrl($url);
+    return $app['twig']->render('post.twig', array(
+        'post' => $post,
+    ));
+});
+
+$app->run();
